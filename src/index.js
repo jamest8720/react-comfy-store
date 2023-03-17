e360c4488bb1a9d781c5a579867fc5659df16482
TA <adam@TA-MacBook-Air.local>
2023-03-17 10:56:12 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// dev-u1zthesqgorx2pgj.us.auth0.com
+// 48m0Q0wOTN1wLOtqhCsZPfdDEiZCAHkc
+
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="About" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
+            exercitationem veritatis similique perferendis blanditiis maxime,
+            aliquam, corporis odit dolores illum amet aut? Possimus ex nemo
+            ratione provident qui eius ab beatae omnis voluptatibus quasi,
+            similique nesciunt consectetur dolore fugiat laudantium libero
+            officia magni dignissimos. Ab sequi eos a ipsam praesentium soluta
+            et minus, dolore deserunt at nisi non in nulla commodi eligendi, hic
+            necessitatibus, laboriosam quo ea. Dolorem, incidunt exercitationem
+            doloribus necessitatibus quibusdam a, ipsa, soluta ipsum labore
+            adipisci sequi!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
